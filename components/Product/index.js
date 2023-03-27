a5cbaf1cd39fccba29c7743b9c1d738a5de563bb
Ella Kempel <ella.lernt@mail.de>
2023-03-27 17:37:34 +0200

    add solution

--- components/Product/index.js
+++ components/Product/index.js
@@ -3,8 +3,11 @@ import { useRouter } from "next/router";
 import { StyledButton } from "../Button/Button.styled";
 import { ProductCard } from "./Product.styled";
 import Comments from "../Comments";
+import ProductForm from "../ProductForm";
+import { useState } from "react";
 
-export default function Product() {
+export default function Product({ onSubmit, onDelete }) {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
@@ -24,6 +27,29 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        ✏️
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          onDelete(id);
+        }}
+      >
+        ❌
+      </button>
+      {isEditMode && (
+        <ProductForm
+          onSubmit={onSubmit}
+          value={data.name}
+          isEditMode={isEditMode}
+        />
+      )}
       <StyledButton type="button" onClick={() => router.push("/")}>
         Back to all
       </StyledButton>
--- pages/[id].js
+++ pages/[id].js
@@ -1,9 +1,63 @@
 import Product from "../components/product";
+import { useRouter } from "next/router";
+import useSWRMutation from "swr/mutation";
 
 export default function ProductDetailsPage() {
+  const router = useRouter();
+  const {
+    query: { id },
+    push,
+  } = router;
+
+  async function updateProduct(url, { arg }) {
+    const response = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(arg),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      await response.json();
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
+
+  const { trigger, isMutating } = useSWRMutation(
+    `/api/products/${id}`,
+    updateProduct
+  );
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const jokeData = Object.fromEntries(formData);
+    await trigger(jokeData);
+    push("/");
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      await response.json();
+      push("/");
+    } else {
+      console.error(`Error: ${response.status}`);
+    }
+  }
+
+  if (isMutating) {
+    return <h1>Submitting your changes.</h1>;
+  }
+
   return (
     <>
-      <Product />
+      <Product onSubmit={handleEditProduct} onDelete={handleDeleteProduct} />
     </>
   );
 }
